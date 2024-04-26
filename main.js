f5a8425e1101803893f5bb7993f656ca5214d21c
Parthi-Jain <156905021+Parthi-Jain@users.noreply.github.com>
2024-04-26 16:53:23 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+var first_name = "Parthi ";
+var second_name = "Jain";
+var full_name = first_name.concat(second_name);
+
+console.log(full_name);
+
+var a = 10;
+var b = 5;
+
+var add = a + b;
+
+console.log(add);
+
+var subtract = a - b;
+
+console.log(subtract);
+
+var multipy = a * b;
+
+console.log(multipy);
+
+var divide = a / b;
+
+console.log(divide);
